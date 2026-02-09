5d17860d43712dddf884c144196445575db710d8
AMRIT PAL SINGH <126061621+amt-12@users.noreply.github.com>
2026-02-09 09:30:58 +0530

    as

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+  <>
+      <div className='bg-[aqua] mx-2.5 py-2.5'>Login
+    
+    
+    
+    </div>  <div style={{backgroundColor:"red"}}>Login
+    
+    
+    
+    </div>
+  </>
+  )
+}
+
+export default Login
